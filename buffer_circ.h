3dd52c4d35bce0f78df9ed44b43f7b1d3735a292
Alejandro Juan Sintes <aleju243@student.liu.se>
2016-12-13 20:56:07 +0100

    first

---
+++ buffer_circ.h
@@ -0,0 +1,16 @@
+#ifndef BUFFER_CIRC_
+#define BUFFER_CIRC_
+
+#define BUFSIZE 10
+int ok; // definir ok 
+
+struct Buffer_Circ *buff;
+void initbuffer (struct Buffer_Circ *buff);
+int get_item (int* x, struct Buffer_Circ *buff);
+int put_item (int x, struct Buffer_Circ *buff);
+char* bc_vacio (struct Buffer_Circ *buff);
+char* bc_lleno (struct Buffer_Circ *buff);
+void print (struct Buffer_Circ *buff);
+int num_elementos (struct Buffer_Circ *buff);
+
+#endif //
